f608f8a1ac27068a71162980db5f777472f70dd2
GuilhermeSergio <gui.sergiiio@gmail.com>
2023-01-17 16:01:29 -0300

    novo commit

--- src/main/java/com/guilhermesergio/workshopmongo/domain/Post.java
+++ src/main/java/com/guilhermesergio/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.guilhermesergio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.guilhermesergio.workshopmongo.dto.AuthorDTO;
+import com.guilhermesergio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {	
 	}
@@ -69,6 +74,9 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -86,4 +94,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
